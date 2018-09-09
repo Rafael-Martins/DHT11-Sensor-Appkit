48b870ff1dab552a50c292395661457311fbb136
Unknown <rafaelplmartins@hotmail.com>
2018-09-09 20:17:52 -0300

    feat: adds the http, and parser on api

--- js/api/main.js
+++ js/api/main.js
@@ -1,10 +1,20 @@
 const SerialPort = require('serialport');
 const Readline = SerialPort.parsers.Readline;
 const parser = new Readline();
+const express = require('express');
+const app = express();
+
+app.use(function(req, res, next) { // Enable cors
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const mySerial = new SerialPort('/dev/ttyUSB0', {
   baudRate: 9600
 });
+
+let temp = {}
 
 mySerial.pipe(parser);
 
@@ -13,10 +23,19 @@ mySerial.on('open', function () {
 });
 
 mySerial.on('data', function (data) {
-  console.log(data)
   console.log(data.toString());
+  const dataParsed = data.toString().split("/")
+  temp = {temp: dataParsed[0], hum: dataParsed[1]}
 });
 
 mySerial.on('err', function (data) {
   console.log(err.message);
+});
+
+app.get('/', function (req, res) {
+  res.send(temp);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
